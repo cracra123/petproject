05db10e1f5bf79be33daca247be3c411d12e8abb
Adel Avzalov <warwara595@mail.ru>
2024-11-11 23:18:30 +0300

    asdasdasdsa

--- packages/backend/controllers/search_controller.js
+++ packages/backend/controllers/search_controller.js
@@ -5,6 +5,7 @@ import { UserService } from "../prisma/UserService.js";
 const SearchController = express.Router();
 
 SearchController.post("/", async (req, res) => {
+  console.log(req.body.input);
   const { input } = req.body;
   console.log(input);
   const result = await UserService.findAllUsersByLogin({
